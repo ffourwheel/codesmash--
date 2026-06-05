eaf400dc49cb640ee8b9fb4d25c449ded4200f4d
tanakron1748q <tanakron1748@gmail.com>
2026-06-05 10:24:17 +0700

    setup : Project

---
+++ codesmah-new-gen/src/app/(app)/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div className="flex min-h-screen flex-col item-center justify-center:">
+      <h1 className="flex text-black text-center">helo world</h1>
+    </div>
+  )
+}
